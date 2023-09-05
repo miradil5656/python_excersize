b55861d4df5bbc2006821a5a21abed94515777ca
Miradil <miradild404@gmail.com>
2023-09-05 16:08:54 +0900

    add P_e_01.py and P_e_02.py

---
+++ P_e_02.py
@@ -0,0 +1,9 @@
+#2.  Write a Python program to find out what version of python you are using.
+
+# Solution:
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
+
